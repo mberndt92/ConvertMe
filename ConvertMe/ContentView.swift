a01be00f716360e56e9997bb3ef925e8504c06b5
mberndt92 <maximilian_berndt@icloud.com>
2023-03-08 08:36:35 +0900

    Convert me (Temperature)

--- ConvertMe/ContentView.swift
+++ ConvertMe/ContentView.swift
@@ -8,12 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var fromTemperature: TemperatureUnit = TemperatureUnit.Celsius
+    @State private var toTemperature: TemperatureUnit = TemperatureUnit.Fahrenheit
+    @State private var temperature: Double = 32.0
+    
+    enum TemperatureUnit: String, CaseIterable {
+        case Celsius
+        case Kelvin
+        case Fahrenheit
+    }
+    
+    // only 1 unit was required to be converted in this project. Let's keep it simple but I might add it later
+    enum LengthUnit: String {
+        case Kilometers
+        case Feet
+        case Yard
+        case Miles
+    }
+    
+    enum TimeUnit: String {
+        case Seconds
+        case Minutes
+        case Hours
+        case Days
+    }
+    
+    enum VolumeUnit: String {
+        case Milliliters
+        case Liters
+        case Cups
+        case Pints
+        case Gallons
+    }
+
+    private func temperatureConversion(
+        value: Double,
+        from: TemperatureUnit,
+        to: TemperatureUnit
+    ) -> Double {
+        switch (from, to) {
+        case (.Celsius, .Kelvin): return temperature + 273.15
+        case (.Kelvin, .Celsius): return temperature - 273.15
+        case (.Celsius, .Fahrenheit): return (temperature * 9 / 5) + 32
+        case (.Fahrenheit, .Celsius): return (temperature - 32) * 5 / 9
+        case (.Kelvin, .Fahrenheit): return ((temperature - 273.15) * 9 / 5) + 32
+        case (.Fahrenheit, .Kelvin): return ((temperature - 32) * 5 / 9) + 273.15
+        default: return temperature
+        }
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            Section {
+                Picker("Convert From", selection: $fromTemperature) {
+                    ForEach(TemperatureUnit.allCases, id: \.self) {
+                        Text("\($0.rawValue)")
+                    }
+                }
+                Picker("Convert To", selection: $toTemperature) {
+                    ForEach(TemperatureUnit.allCases, id: \.self) {
+                        Text("\($0.rawValue)")
+                    }
+                }
+            } header: {
+                Text("Temperature Conversion")
+            }
+            Section {
+                TextField("Temperature", value: $temperature, format: .number)
+            } header: {
+                Text("Original Temperature")
+            }
+            Section {
+                Text(temperatureConversion(
+                    value:temperature,
+                    from: fromTemperature,
+                    to: toTemperature
+                ), format: .number)
+            } header: {
+                Text("Converted Temperature")
+            }
         }
         .padding()
     }
