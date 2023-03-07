ba8ee570169a73fc768fae88105537b1364808ce
mberndt92 <maximilian_berndt@icloud.com>
2023-03-08 07:57:08 +0900

    Initial Commit

---
+++ ConvertMe/ConvertMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertMeApp.swift
+//  ConvertMe
+//
+//  Created by Maximilian Berndt on 2023/03/08.
+//
+
+import SwiftUI
+
+@main
+struct ConvertMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
